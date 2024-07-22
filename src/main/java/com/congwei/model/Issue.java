439590cdc5d129af9c6534b924de95786167f03e
congweiH <congwei_huang@163.com>
2024-07-23 00:47:50 +0800

    实体类声明

--- src/main/java/com/congwei/model/Issue.java
+++ src/main/java/com/congwei/model/Issue.java
@@ -1,7 +1,12 @@
 package com.congwei.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -10,6 +15,23 @@ public class Issue {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String title;
+    private String description;
+    private String status;
+    private Long projectId;
+    private String priority;
+    private LocalDate dueDate;
+    @ElementCollection
+    private List<String> tags = new ArrayList<>();
+
     @ManyToOne
     private User assignee;
+
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
